fe3ad879ab3b0031459aee0f842101e14d5eec66
Malach Salama <legitdiamonds050@gmail.com>
2021-11-15 14:13:56 +0300

    finished hamburger menu functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnHamburger = document.querySelector(".menu-btn");
 const navMenu = document.querySelector(".nav--links");
+const hamburger = document.querySelector(".hamburger");
 
 // Modal window
 const openModal = function (e) {
@@ -32,20 +33,27 @@ document.addEventListener("keydown", function (e) {
 
 // Hamburger menu
 const openMenu = function () {
-  navMenu.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  navMenu.classList.toggle("nav-links-show");
+  overlay.classList.toggle("hidden");
+
+  if (navMenu.classList.contains("nav-links-show")) {
+    hamburger.src = "/assets/images/icon-close-menu.svg";
+  } else {
+    hamburger.src = "/assets/images/icon-hamburger.svg";
+  }
 };
 
 const closeMenu = function () {
+  navMenu.classList.remove("nav-links-show");
+  hamburger.src = "/assets/images/icon-hamburger.svg";
   overlay.classList.add("hidden");
-  navMenu.classList.add("hidden");
 };
 
 btnHamburger.addEventListener("click", openMenu);
 overlay.addEventListener("click", closeMenu);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+  if (e.key === "Escape" && !navMenu.classList.contains("hidden")) {
     closeMenu();
   }
 });
